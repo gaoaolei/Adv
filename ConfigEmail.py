38f2bdc4761a294e20a4fd55d92eed932061f4cd
DESKTOP-RG0DRJU\admin <853573584@qq.com>
2021-02-20 17:59:33 +0800

    重新整理一遍

--- ConfigEmail.py
+++ ConfigEmail.py
@@ -11,9 +11,9 @@ from_address = "853573584@qq.com"  # 用户名
 mail_pass = "xkoxcpyswmyubcie"  # 口令
 
 '''调试模式'''
-to_address = ['gaoaolei@qimao.com']
+# to_address = ['gaoaolei@qimao.com']
 '''正是模式'''
-# to_address = ['gaoaolei@qimao.com', 'zhuguixin@qimao.com', 'shenyufan@qimao.com']
+to_address = ['gaoaolei@qimao.com', 'zhuguixin@qimao.com', 'shenyufan@qimao.com']
 
 
 def send_mail(content):
